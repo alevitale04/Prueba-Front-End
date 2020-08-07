0b6c5a5ae703ff0a1206ef82d2c1b066cb783029
Alejandro Vitale <alevitale04@gmail.com>
2020-08-07 00:19:13 -0300

    Se crea funcion para calcular los dias transcurridos desde la fecha de publicacion

--- js/funcional.js
+++ js/funcional.js
@@ -21,14 +21,14 @@ const crearAviso = (posting_id,address,zone,city,operation_type,price_amount,pri
 
 const aviso1 = crearAviso(   
     "44557981","Guido 1800","Recoleta","Capital Federal",1,"13500","ARS","3500","ARS",1,"Alquiler","SIMPLE",
-    "23/01/2020","AVAILABLE","Disponible","Guido y Callao, 2amb coc y lav todo luz 50m impec amob categoría ",
+    "2020/01/23","AVAILABLE","Disponible","Guido y Callao, 2amb coc y lav todo luz 50m impec amob categoría ",
     "imagenes/aviso1/0.jpg","guido-y-callao-2amb-coc-y-lav-todo-luz-50m-impec-44557981",
     "Sed in felis nec lorem imperdiet euismod. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse lectus mi, imperdiet et venenatis pulvinar, mattis id orci. In ut aliquam orci. Cras vitae risus posuere, ullamcorper erat vitae, tempor libero. Nulla placerat euismod lectus et maximus. Duis non magna mattis, mattis neque eu, dictum dui. Aliquam aliquam fermentum purus quis placerat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum sit amet ligula odio. Integer id tempor ipsum. Phasellus maximus quam felis, id vulputate massa ullamcorper id."
 );
 
 const aviso2 = crearAviso(
     "44554144","San Lorenzo 1300","Güemes","Mar Del Plata",2,"110500","USD","3000", "ARS",
-    2, "Venta","SUPERHIGHLIGHTED","18/07/2019","AVAILABLE","Disponible",
+    2, "Venta","SUPERHIGHLIGHTED","2019/07/18","AVAILABLE","Disponible",
     "Dueño San Lorenzo/Güemes. LC  2 d, a balcón terraza a jardín. A nvo. cochera",
     "imagenes/aviso2/0.jpg",
     "dueno-san-lorenzo-guemes.-lc-2-d-a-balcon-terraz-44554144",
@@ -37,7 +37,7 @@ const aviso2 = crearAviso(
 
 const aviso3 = crearAviso(
     "44186948","Juncal 3000","Barrio Norte","Capital Federal",3,"22500","ARS",'0','0',3,"Alquiler Temporal","HIGHLIGHTED",
-    "02/12/2019","RESERVED","Reservado","Juncal/Coronel Díaz. Al frente, 63m, gran balcón terraza. Todo luz",
+    "2019/12/02","RESERVED","Reservado","Juncal/Coronel Díaz. Al frente, 63m, gran balcón terraza. Todo luz",
     "imagenes/aviso3/0.jpg",
     "juncal-coronel-diaz.-al-frente-63m-gran-balcon-t-44186948",
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus a neque non magna scelerisque imperdiet sed ac magna. Mauris enim lectus, varius at quam a, venenatis condimentum ante. Morbi mattis velit vel dictum varius. Suspendisse tempor velit ac rhoncus vehicula. Aenean elementum dolor purus, et mattis arcu consectetur a. Sed dictum felis id molestie accumsan. Aliquam sit amet diam feugiat, tincidunt sem vehicula, dignissim augue. Integer gravida sapien erat, in faucibus ligula cursus non. Aliquam vestibulum est nibh, vitae vehicula ipsum facilisis quis. Pellentesque ac tempus metus, non posuere dolor."
@@ -272,3 +272,28 @@ CajadeAvisos(aviso2);
 CajadeAvisos(aviso3);
 CajadeAvisos(aviso1);
 
+const tiempoPublicado = (datos) =>{
+
+let fecha = datos.publish_date;
+fecha = new Date(fecha);
+
+const fechaactual = new Date();
+
+const inicio =  new Date(fecha).getTime();
+
+const final =  new Date(fechaactual).getTime();
+
+
+const diferencia = final - inicio;
+
+return parseInt((diferencia/(1000*60*60*24)));
+
+};
+
+console.log(tiempoPublicado(aviso2))
+
+
+
+
+                 
+                
